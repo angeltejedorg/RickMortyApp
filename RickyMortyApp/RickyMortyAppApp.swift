299ec037fd1383339553fdce06e47fe7106ae94f
Angel Tejedor <angeltejedorg@gmail.com>
2026-01-14 00:54:03 -0500

    Initial Commit

---
+++ RickyMortyApp/RickyMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyMortyAppApp.swift
+//  RickyMortyApp
+//
+//  Created by Angel Tejedor on 14/01/26.
+//
+
+import SwiftUI
+
+@main
+struct RickyMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
